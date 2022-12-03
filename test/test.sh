0bbfb0f556a3a8125fb3fe1edb40d26ea1488110
Nicuvëo <crucuny@gmail.com>
2022-12-03 19:08:34 +0000

    Rewrote test script.

--- test/test.sh
+++ test/test.sh
@@ -10,44 +10,53 @@ if [ $# -ne 0 ]; then
 fi
 
 stack build
+if [ $? -ne 0 ]; then
+    echo "failed to compile BFS"
+    exit 1
+fi
+
+failed=0
+
+function run() {
+    source="$1"
+    label="$2"
+    flag="$3"
+
+    test=${source%.*}
+    name=$test-$label
+    bffile=/tmp/$name.bf
+    afile=/tmp/$name.a
+    outfile=/tmp/$name.out
+
+    stack exec bfs -- $flag $source > $bffile
+    if [ $? -ne 0 ] ; then
+        printf "%-32s[\033[1;31mKO\033[0m] failed to transpile\n" $name
+        failed=1
+        return
+    fi
+
+    bfc $bffile 2> /dev/null
+    if [ $? -ne 0 ] ; then
+        printf "%-32s[\033[1;31mKO\033[0m] failed to compile\n" $name
+        failed=1
+        return
+    fi
+    mv a.out $afile
+
+    elapsed=`{ time $afile < $test.in > $outfile; } 2>&1 | sed -n 's/real\t*//p'`
+    if [ -n "$(diff $test.out $outfile)" ] ; then
+        printf "%-32s[\033[1;31mKO\033[0m] output diff:\n" $name
+        diff $test.out $outfile
+        failed=1
+        return
+    fi
+
+    printf "%-32s[\033[1;32mOK\033[0m] (in %s) \n" $name "$elapsed"
+}
 
 for source in *.bs ; do
-    testname=${source%.*}
-    input=$testname.in
-    output=$testname.out
-
-    ok=1
-    stack exec bfs --    $source > /tmp/$testname.1.bf
-    if [ $? -ne 0 ] ; then
-        printf "%-20s[\033[1;31mKO\033[0m] failed to compile default version\n" $testname
-        ok=0
-    fi
-    stack exec bfs -- -O $source > /tmp/$testname.2.bf
-    if [ $? -ne 0 ] ; then
-        printf "%-20s[\033[1;31mKO\033[0m] failed to compile optimized version\n" $testname
-        ok=0
-    fi
-
-    if [ $ok -eq 1 ] ; then
-        bfc /tmp/$testname.1.bf 2> /dev/null; mv a.out /tmp/$testname.1
-        bfc /tmp/$testname.2.bf 2> /dev/null; mv a.out /tmp/$testname.2
-
-        cat $input | /tmp/$testname.1 > /tmp/$testname.1.out
-        cat $input | /tmp/$testname.2 > /tmp/$testname.2.out
-
-        ok=1
-        if [ -n "$(diff $output /tmp/$testname.1.out)" ] ; then
-            printf "%-20s[\033[1;31mKO\033[0m] output differs for default version\n" $testname
-            cat /tmp/$testname.1.out
-            ok=0
-        fi
-        if [ -n "$(diff $output /tmp/$testname.2.out)" ] ; then
-            printf "%-20s[\033[1;31mKO\033[0m] output differs for optimized version\n" $testname
-            cat /tmp/$testname.2.out
-            ok=0
-        fi
-        if [ $ok -eq 1 ] ; then
-            printf "%-20s[\033[1;32mOK\033[0m]\n" $testname
-        fi
-    fi
+    run "$source" "verbose" ""
+    run "$source" "dense"   "-O"
 done
+
+exit $failed
